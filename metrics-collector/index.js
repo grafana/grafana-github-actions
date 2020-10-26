3e7a4e39bacc2786b99529c178f0cde7c2750c6b
Torkel Ödegaard <torkel@grafana.com>
2020-10-26 18:32:53 +0100

    Move queries to config file

--- metrics-collector/index.js
+++ metrics-collector/index.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const utils_1 = require("../common/utils");
 const Action_1 = require("../common/Action");
 const telemetry_1 = require("../common/telemetry");
 class MetricsCollector extends Action_1.Action {
@@ -14,17 +15,10 @@ class MetricsCollector extends Action_1.Action {
         if (typeLabel) {
             labels['type'] = typeLabel.substr(5);
         }
-        telemetry_1.aiHandle === null || telemetry_1.aiHandle === void 0 ? void 0 : telemetry_1.aiHandle.trackMetric({
-            name: 'issue.closed_count',
-            value: 1,
-            labels,
-        });
+        telemetry_1.aiHandle === null || telemetry_1.aiHandle === void 0 ? void 0 : telemetry_1.aiHandle.trackMetric({ name: 'issue.closed_count', value: 1, labels });
     }
     async onOpened(_issue) {
-        telemetry_1.aiHandle === null || telemetry_1.aiHandle === void 0 ? void 0 : telemetry_1.aiHandle.trackMetric({
-            name: 'issue.opened_count',
-            value: 1,
-        });
+        telemetry_1.aiHandle === null || telemetry_1.aiHandle === void 0 ? void 0 : telemetry_1.aiHandle.trackMetric({ name: 'issue.opened_count', value: 1 });
     }
     async onTriggered(octokit) {
         const repo = await octokit.getRepoInfo();
@@ -37,12 +31,20 @@ class MetricsCollector extends Action_1.Action {
             value: repo.data.open_issues_count,
             type: 'gauge',
         });
-        await this.countQuery('type_bug', 'label:"type/bug" is:open', octokit);
-        await this.countQuery('needs_investigation', 'label:"needs investigation" is:open', octokit);
-        await this.countQuery('needs_more_info', 'label:"needs more info" is:open', octokit);
-        await this.countQuery('unlabeled', 'is:open is:issue no:label', octokit);
-        await this.countQuery('open_prs', 'is:open is:pr', octokit);
-        await this.countQuery('milestone_7_4_open', 'is:open is:issue milestone:7.4 ', octokit);
+        const configPath = utils_1.getInput('configPath');
+        if (!configPath) {
+            return;
+        }
+        const config = (await octokit.readConfig(configPath));
+        for (const query of config.queries) {
+            await this.countQuery(query.name, query.query, octokit);
+        }
+        // await this.countQuery('type_bug', 'label:"type/bug" is:open', octokit)
+        // await this.countQuery('needs_investigation', 'label:"needs investigation" is:open', octokit)
+        // await this.countQuery('needs_more_info', 'label:"needs more info" is:open', octokit)
+        // await this.countQuery('unlabeled', 'is:open is:issue no:label', octokit)
+        // await this.countQuery('open_prs', 'is:open is:pr', octokit)
+        // await this.countQuery('milestone_7_4_open', 'is:open is:issue milestone:7.4 ', octokit)
     }
     async countQuery(name, query, octokit) {
         let count = 0;
