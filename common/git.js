0bbb5ec70f6f0cc03269ec2e9ac007d7e62827b5
Torkel Ödegaard <torkel@grafana.com>
2020-11-15 12:39:20 +0100

    Updated git user

--- common/git.js
+++ common/git.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const exec_1 = require("@actions/exec");
 async function cloneRepo({ token, owner, repo }) {
     await exec_1.exec('git', ['clone', `https://x-access-token:${token}@github.com/${owner}/${repo}.git`]);
-    await exec_1.exec('git', ['config', '--global', 'user.email', 'github-actions[bot]@users.noreply.github.com']);
-    await exec_1.exec('git', ['config', '--global', 'user.name', 'github-actions[bot]']);
+    await exec_1.exec('git', ['config', '--global', 'user.email', 'bot@grafana.com']);
+    await exec_1.exec('git', ['config', '--global', 'user.name', 'grafanabot']);
 }
 exports.cloneRepo = cloneRepo;
 //# sourceMappingURL=git.js.map
--- common/git.ts
+++ common/git.ts
@@ -8,6 +8,6 @@ export interface CloneProps {
 
 export async function cloneRepo({ token, owner, repo }: CloneProps) {
 	await exec('git', ['clone', `https://x-access-token:${token}@github.com/${owner}/${repo}.git`])
-	await exec('git', ['config', '--global', 'user.email', 'github-actions[bot]@users.noreply.github.com'])
-	await exec('git', ['config', '--global', 'user.name', 'github-actions[bot]'])
+	await exec('git', ['config', '--global', 'user.email', 'bot@grafana.com'])
+	await exec('git', ['config', '--global', 'user.name', 'grafanabot'])
 }
