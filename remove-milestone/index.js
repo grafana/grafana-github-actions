80641a749957a0638eab8c557a6a9eaf0e98f02b
Ashley Harrison <ashley.harrison@grafana.com>
2021-12-14 16:44:21 +0000

    Chore: Update remove milestone to include open pull requests against main (#49)
    Chore: Update remove milestone to include open pull requests against main
    Co-authored-by: Alexandra Vargas <alexa1866@gmail.com>

--- remove-milestone/index.js
+++ remove-milestone/index.js
@@ -15,14 +15,28 @@ class RemoveMilestone extends Action_1.Action {
             throw new Error('Missing version input');
         }
         for (const issue of await getIssuesForVersion(octokit, version)) {
-            octokit.octokit.issues.update({
+            await octokit.octokit.issues.update({
                 owner,
                 repo,
                 issue_number: issue.number,
                 milestone: null,
             });
-            octokit.octokit.issues.createComment({
+            await octokit.octokit.issues.createComment({
                 body: `This issue was removed from the ${version} milestone because ${version} is currently being released.`,
+                issue_number: issue.number,
+                owner,
+                repo,
+            });
+        }
+        for (const issue of await getPullRequestsForVersion(octokit, version)) {
+            await octokit.octokit.issues.update({
+                owner,
+                repo,
+                issue_number: issue.number,
+                milestone: null,
+            });
+            await octokit.octokit.issues.createComment({
+                body: `This pull request was removed from the ${version} milestone because ${version} is currently being released.`,
                 issue_number: issue.number,
                 owner,
                 repo,
@@ -39,5 +53,14 @@ async function getIssuesForVersion(octokit, version) {
     }
     return issueList;
 }
-new RemoveMilestone().run();
+async function getPullRequestsForVersion(octokit, version) {
+    const issueList = [];
+    for await (const page of octokit.query({ q: `is:pr is:open milestone:${version} base:main` })) {
+        for (const issue of page) {
+            issueList.push(await issue.getIssue());
+        }
+    }
+    return issueList;
+}
+new RemoveMilestone().run(); // eslint-disable-line
 //# sourceMappingURL=index.js.map
--- remove-milestone/index.ts
+++ remove-milestone/index.ts
@@ -17,15 +17,31 @@ class RemoveMilestone extends Action {
 		}
 
 		for (const issue of await getIssuesForVersion(octokit, version)) {
-			octokit.octokit.issues.update({
+			await octokit.octokit.issues.update({
 				owner,
 				repo,
 				issue_number: issue.number,
 				milestone: null,
 			})
 
-			octokit.octokit.issues.createComment({
+			await octokit.octokit.issues.createComment({
 				body: `This issue was removed from the ${version} milestone because ${version} is currently being released.`,
+				issue_number: issue.number,
+				owner,
+				repo,
+			})
+		}
+
+		for (const issue of await getPullRequestsForVersion(octokit, version)) {
+			await octokit.octokit.issues.update({
+				owner,
+				repo,
+				issue_number: issue.number,
+				milestone: null,
+			})
+
+			await octokit.octokit.issues.createComment({
+				body: `This pull request was removed from the ${version} milestone because ${version} is currently being released.`,
 				issue_number: issue.number,
 				owner,
 				repo,
@@ -46,4 +62,16 @@ async function getIssuesForVersion(octokit: OctoKit, version: any): Promise<Issu
 	return issueList
 }
 
-new RemoveMilestone().run()
+async function getPullRequestsForVersion(octokit: OctoKit, version: any): Promise<Issue[]> {
+	const issueList = []
+
+	for await (const page of octokit.query({ q: `is:pr is:open milestone:${version} base:main` })) {
+		for (const issue of page) {
+			issueList.push(await issue.getIssue())
+		}
+	}
+
+	return issueList
+}
+
+new RemoveMilestone().run() // eslint-disable-line
