4fdbe70d95e21cb452e6628db396a27386c07d7a
Ashley Harrison <ashley.harrison@grafana.com>
2021-12-14 11:11:02 +0000

    user essentials mob! :trident:

--- remove-milestone/index.ts
+++ remove-milestone/index.ts
@@ -7,15 +7,32 @@ import { exec } from '@actions/exec'
 import { cloneRepo } from '../common/git'
 import { OctoKit } from '../api/octokit'
 import { EventPayloads } from '@octokit/webhooks'
+import { Issue } from '../api/api'
 
 class RemoveMilestone extends Action {
 	id = 'RemoveMilestone'
 
 	async onTriggered(octokit: OctoKit) {
 		const { owner, repo } = context.repo
+		const payload = context.payload as EventPayloads.WebhookPayloadWorkflowDispatch
+		const version = (payload.inputs as any).version
 
-		octokit.octokit.issues.deleteMilestone()
+		for (const issue of await this.getIssuesForVersion(octokit, version)) {
+    }
 	}
+
+  async getIssuesForVersion(octokit: OctoKit, version: any): Promise<Issue[]> {
+    const issueList = []
+
+    for await (const page of octokit.query({ q: `is:issue is:open milestone:${version}` })) {
+      for (const issue of page) {
+        issueList.push(await issue.getIssue())
+      }
+    }
+
+    return issueList
+  }
+
 }
 
 
