eb7f88132e9cd22e051f9698c7823ab570706df2
Ashley Harrison <ashley.harrison@grafana.com>
2021-12-14 15:16:35 +0000

    Chore: Create new github action to remove milestone from issue (#48)
    Chore: Create new github action to remove milestone from issue
    
    Co-authored-by: Alexandra Vargas <alexa1866@gmail.com>

---
+++ remove-milestone/index.js
@@ -0,0 +1,43 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const github_1 = require("@actions/github");
+const Action_1 = require("../common/Action");
+class RemoveMilestone extends Action_1.Action {
+    constructor() {
+        super(...arguments);
+        this.id = 'RemoveMilestone';
+    }
+    async onTriggered(octokit) {
+        const { owner, repo } = github_1.context.repo;
+        const payload = github_1.context.payload;
+        const version = payload.inputs.version;
+        if (!version) {
+            throw new Error('Missing version input');
+        }
+        for (const issue of await getIssuesForVersion(octokit, version)) {
+            octokit.octokit.issues.update({
+                owner,
+                repo,
+                issue_number: issue.number,
+                milestone: null,
+            });
+            octokit.octokit.issues.createComment({
+                body: `This issue was removed from the ${version} milestone because ${version} is currently being released.`,
+                issue_number: issue.number,
+                owner,
+                repo,
+            });
+        }
+    }
+}
+async function getIssuesForVersion(octokit, version) {
+    const issueList = [];
+    for await (const page of octokit.query({ q: `is:issue is:open milestone:${version}` })) {
+        for (const issue of page) {
+            issueList.push(await issue.getIssue());
+        }
+    }
+    return issueList;
+}
+new RemoveMilestone().run();
+//# sourceMappingURL=index.js.map
---
+++ remove-milestone/index.ts
@@ -0,0 +1,49 @@
+import { context } from '@actions/github'
+import { Action } from '../common/Action'
+import { OctoKit } from '../api/octokit'
+import { EventPayloads } from '@octokit/webhooks'
+import { Issue } from '../api/api'
+
+class RemoveMilestone extends Action {
+	id = 'RemoveMilestone'
+
+	async onTriggered(octokit: OctoKit) {
+		const { owner, repo } = context.repo
+		const payload = context.payload as EventPayloads.WebhookPayloadWorkflowDispatch
+		const version = (payload.inputs as any).version
+
+		if (!version) {
+			throw new Error('Missing version input')
+		}
+
+		for (const issue of await getIssuesForVersion(octokit, version)) {
+			octokit.octokit.issues.update({
+				owner,
+				repo,
+				issue_number: issue.number,
+				milestone: null,
+			})
+
+			octokit.octokit.issues.createComment({
+				body: `This issue was removed from the ${version} milestone because ${version} is currently being released.`,
+				issue_number: issue.number,
+				owner,
+				repo,
+			})
+		}
+	}
+}
+
+async function getIssuesForVersion(octokit: OctoKit, version: any): Promise<Issue[]> {
+	const issueList = []
+
+	for await (const page of octokit.query({ q: `is:issue is:open milestone:${version}` })) {
+		for (const issue of page) {
+			issueList.push(await issue.getIssue())
+		}
+	}
+
+	return issueList
+}
+
+new RemoveMilestone().run()
