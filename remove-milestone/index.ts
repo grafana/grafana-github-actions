1799bc4f0e7b47a5c149ddfec295630da69115f2
Alexandra Vargas <alexa1866@gmail.com>
2021-12-14 12:20:10 +0100

    user essentials mob! :trident:

--- remove-milestone/index.ts
+++ remove-milestone/index.ts
@@ -1,10 +1,5 @@
-// import { error as logError, getInput, setFailed } from '@actions/core'
 import { context } from '@actions/github'
-// import { EventPayloads } from '@octokit/webhooks'
-// import { OctoKitIssue } from '../api/octokit'
 import { Action } from '../common/Action'
-import { exec } from '@actions/exec'
-import { cloneRepo } from '../common/git'
 import { OctoKit } from '../api/octokit'
 import { EventPayloads } from '@octokit/webhooks'
 import { Issue } from '../api/api'
@@ -17,23 +12,27 @@ class RemoveMilestone extends Action {
 		const payload = context.payload as EventPayloads.WebhookPayloadWorkflowDispatch
 		const version = (payload.inputs as any).version
 
-		for (const issue of await this.getIssuesForVersion(octokit, version)) {
-    }
+		for (const issue of await getIssuesForVersion(octokit, version)) {
+			octokit.octokit.issues.update({
+				owner,
+				repo,
+				issue_number: issue.number,
+				milestone: null,
+			})
+		}
 	}
-
-  async getIssuesForVersion(octokit: OctoKit, version: any): Promise<Issue[]> {
-    const issueList = []
-
-    for await (const page of octokit.query({ q: `is:issue is:open milestone:${version}` })) {
-      for (const issue of page) {
-        issueList.push(await issue.getIssue())
-      }
-    }
-
-    return issueList
-  }
-
 }
 
+async function getIssuesForVersion(octokit: OctoKit, version: any): Promise<Issue[]> {
+	const issueList = []
+
+	for await (const page of octokit.query({ q: `is:issue is:open milestone:${version}` })) {
+		for (const issue of page) {
+			issueList.push(await issue.getIssue())
+		}
+	}
+
+	return issueList
+}
 
 new RemoveMilestone().run()
