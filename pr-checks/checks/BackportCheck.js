e08b3bd0a68c074877227b8e427c3d4804113541
Marcus Efraimsson <marcus.efraimsson@gmail.com>
2021-12-15 19:51:37 +0100

    Adds backport pr check

---
+++ pr-checks/checks/BackportCheck.js
@@ -0,0 +1,90 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.BackportCheck = void 0;
+const github_1 = require("@actions/github");
+const Check_1 = require("../Check");
+const labelRegExp = /^backport ([^ ]+)(?: ([^ ]+))?$/;
+class BackportCheck extends Check_1.Check {
+    constructor(config) {
+        super();
+        this.config = config;
+        this.id = 'backport';
+    }
+    subscribe(s) {
+        s.on(['pull_request', 'pull_request_target'], 'labeled', async (ctx) => {
+            const payload = github_1.context.payload;
+            if (!payload.label) {
+                return;
+            }
+            const labelAdded = payload.label.name;
+            console.log('Label added', labelAdded, payload.pull_request.labels);
+            const matches = labelRegExp.exec(labelAdded);
+            if (matches !== null) {
+                return this.successEnabled(ctx, payload.pull_request.head.sha);
+            }
+            if (this.config.skipLabels) {
+                for (let n = 0; n < this.config.skipLabels.length; n++) {
+                    const l = this.config.skipLabels[n];
+                    if (l === labelAdded) {
+                        return this.successSkip(ctx, payload.pull_request.head.sha);
+                    }
+                }
+            }
+            for (let n = 0; n < payload.pull_request.labels.length; n++) {
+                const existingLabel = payload.pull_request.labels[n];
+                const matches = labelRegExp.exec(existingLabel.name);
+                if (matches !== null) {
+                    return this.successEnabled(ctx, payload.pull_request.head.sha);
+                }
+                if (this.config.skipLabels) {
+                    for (let n = 0; n < this.config.skipLabels.length; n++) {
+                        const l = this.config.skipLabels[n];
+                        if (l === existingLabel.name) {
+                            return this.successSkip(ctx, payload.pull_request.head.sha);
+                        }
+                    }
+                }
+            }
+        });
+        s.on(['pull_request', 'pull_request_target'], 'unlabeled', async (ctx) => {
+            const payload = github_1.context.payload;
+            if (!payload.label) {
+                return;
+            }
+            console.log('Label removed', payload.label.name, payload.pull_request.labels);
+            for (let n = 0; n < payload.pull_request.labels.length; n++) {
+                const existingLabel = payload.pull_request.labels[n];
+                if (this.config.skipLabels) {
+                    for (let n = 0; n < this.config.skipLabels.length; n++) {
+                        const l = this.config.skipLabels[n];
+                        if (l === existingLabel.name) {
+                            return this.successSkip(ctx, payload.pull_request.head.sha);
+                        }
+                    }
+                }
+            }
+            const labelRemoved = payload.label.name;
+            const matches = labelRegExp.exec(labelRemoved);
+            if (matches !== null) {
+                return this.failure(ctx, payload.pull_request.head.sha);
+            }
+        });
+    }
+    successEnabled(ctx, sha) {
+        const title = this.config.title ?? 'Backport Check';
+        const description = this.config.backportEnabled ?? 'Backport enabled';
+        return ctx.success({ sha, title, description, targetURL: this.config.targetUrl });
+    }
+    successSkip(ctx, sha) {
+        const title = this.config.title ?? 'Backport Check';
+        const description = this.config.backportSkipped ?? 'Backport skipped';
+        return ctx.success({ sha, title, description, targetURL: this.config.targetUrl });
+    }
+    failure(ctx, sha) {
+        const title = this.config.title ?? 'Backport Check';
+        const description = this.config.failure ?? 'Backport decision needed';
+        return ctx.failure({ sha, title, description, targetURL: this.config.targetUrl });
+    }
+}
+exports.BackportCheck = BackportCheck;
+//# sourceMappingURL=BackportCheck.js.map
---
+++ pr-checks/checks/BackportCheck.ts
@@ -0,0 +1,110 @@
+import { context } from '@actions/github'
+import { EventPayloads } from '@octokit/webhooks'
+import { Check } from '../Check'
+import { CheckContext, CheckSubscriber } from '../types'
+
+export type BackportCheckConfig = {
+	title?: string
+	targetUrl?: string
+	backportEnabled?: string
+	backportSkipped?: string
+	failure?: string
+	skipLabels?: string[]
+}
+
+const labelRegExp = /^backport ([^ ]+)(?: ([^ ]+))?$/
+
+export class BackportCheck extends Check {
+	id = 'backport'
+
+	constructor(private config: BackportCheckConfig) {
+		super()
+	}
+
+	subscribe(s: CheckSubscriber) {
+		s.on(['pull_request', 'pull_request_target'], 'labeled', async (ctx) => {
+			const payload = context.payload as EventPayloads.WebhookPayloadPullRequest
+			if (!payload.label) {
+				return
+			}
+			const labelAdded = payload.label.name
+			console.log('Label added', labelAdded, payload.pull_request.labels)
+
+			const matches = labelRegExp.exec(labelAdded)
+			if (matches !== null) {
+				return this.successEnabled(ctx, payload.pull_request.head.sha)
+			}
+
+			if (this.config.skipLabels) {
+				for (let n = 0; n < this.config.skipLabels.length; n++) {
+					const l = this.config.skipLabels[n]
+					if (l === labelAdded) {
+						return this.successSkip(ctx, payload.pull_request.head.sha)
+					}
+				}
+			}
+
+			for (let n = 0; n < payload.pull_request.labels.length; n++) {
+				const existingLabel = payload.pull_request.labels[n]
+				const matches = labelRegExp.exec(existingLabel.name)
+				if (matches !== null) {
+					return this.successEnabled(ctx, payload.pull_request.head.sha)
+				}
+
+				if (this.config.skipLabels) {
+					for (let n = 0; n < this.config.skipLabels.length; n++) {
+						const l = this.config.skipLabels[n]
+						if (l === existingLabel.name) {
+							return this.successSkip(ctx, payload.pull_request.head.sha)
+						}
+					}
+				}
+			}
+		})
+
+		s.on(['pull_request', 'pull_request_target'], 'unlabeled', async (ctx) => {
+			const payload = context.payload as EventPayloads.WebhookPayloadPullRequest
+			if (!payload.label) {
+				return
+			}
+
+			console.log('Label removed', payload.label.name, payload.pull_request.labels)
+
+			for (let n = 0; n < payload.pull_request.labels.length; n++) {
+				const existingLabel = payload.pull_request.labels[n]
+				if (this.config.skipLabels) {
+					for (let n = 0; n < this.config.skipLabels.length; n++) {
+						const l = this.config.skipLabels[n]
+						if (l === existingLabel.name) {
+							return this.successSkip(ctx, payload.pull_request.head.sha)
+						}
+					}
+				}
+			}
+
+			const labelRemoved = payload.label.name
+			const matches = labelRegExp.exec(labelRemoved)
+			if (matches !== null) {
+				return this.failure(ctx, payload.pull_request.head.sha)
+			}
+		})
+	}
+
+	private successEnabled(ctx: CheckContext, sha: string) {
+		const title = this.config.title ?? 'Backport Check'
+		const description = this.config.backportEnabled ?? 'Backport enabled'
+		return ctx.success({ sha, title, description, targetURL: this.config.targetUrl })
+	}
+
+	private successSkip(ctx: CheckContext, sha: string) {
+		const title = this.config.title ?? 'Backport Check'
+		const description = this.config.backportSkipped ?? 'Backport skipped'
+		return ctx.success({ sha, title, description, targetURL: this.config.targetUrl })
+	}
+
+	private failure(ctx: CheckContext, sha: string) {
+		const title = this.config.title ?? 'Backport Check'
+		const description = this.config.failure ?? 'Backport decision needed'
+		return ctx.failure({ sha, title, description, targetURL: this.config.targetUrl })
+	}
+}
