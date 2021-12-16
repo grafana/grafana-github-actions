f9a7812fd2e21d52c128962a06417446d18c3ace
Hugo Häggmark <hugo.haggmark@grafana.com>
2021-12-16 12:51:17 +0100

    Update update-changelog/index.ts
    Co-authored-by: Maria Alexandra <239999+axelavargas@users.noreply.github.com>

--- update-changelog/index.ts
+++ update-changelog/index.ts
@@ -64,7 +64,7 @@ class UpdateChangelog extends Action {
 
 		await octokit.octokit.pulls.create({
 			base: 'main',
-			body: 'This exciting! So much has changed!\nDO NOT CHANGE THE TITLES DIRECTLY IN THIS PR, everyting in the PR is auto generated.',
+			body: 'This exciting! So much has changed!\nDO NOT CHANGE THE TITLES DIRECTLY IN THIS PR, everything in the PR is auto-generated.',
 			head: branchName,
 			owner,
 			repo,
