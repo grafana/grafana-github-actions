4540329f64a2392af31730130d0cc2d22354ce50
Martin Disibio <mdisibio@gmail.com>
2022-02-28 15:22:14 -0500

    lint

--- docs-target/index.ts
+++ docs-target/index.ts
@@ -1,26 +1,20 @@
 import { setFailed } from '@actions/core'
 import { getRequiredInput } from '../common/utils'
-import { setOutput} from '@actions/core'
+import { setOutput } from '@actions/core'
 
-let forcePrefix = "v"
-let trimPrefixes : string[] = [
-	"release-",
-	"v",
-]
+let forcePrefix = 'v'
+let trimPrefixes: string[] = ['release-', 'v']
+let knownRefs = new Map<string, string>([['main', 'next']])
 
-let knownRefs = new Map<string,string> ([
-	["main", "next"],
-])
-
-function setTarget(target : string) {
-	console.log("Output target: " + target)
-	setOutput("target", target)
+function setTarget(target: string) {
+	console.log('Output target: ' + target)
+	setOutput('target', target)
 }
 
 function run() {
 	try {
-		var ref_name : string = getRequiredInput("ref_name")
-		console.log("Input ref_name: " + ref_name)
+		var ref_name: string = getRequiredInput('ref_name')
+		console.log('Input ref_name: ' + ref_name)
 
 		var target = knownRefs.get(ref_name)
 		if (target != undefined) {
@@ -28,24 +22,24 @@ function run() {
 			return
 		}
 
-		trimPrefixes.forEach(prefix => {
+		trimPrefixes.forEach((prefix) => {
 			if (ref_name.startsWith(prefix)) {
 				ref_name = ref_name.slice(prefix.length)
 			}
-		});
+		})
 
 		// The node semver package can't parse things like "x.y" without patch
 		// https://github.com/npm/node-semver/issues/164#issuecomment-247157991
 		// So doing cheap way to get major.minor
-		let parts = ref_name.split(".", 2)
+		let parts = ref_name.split('.', 2)
 		if (parts.length != 2) {
-			throw "ref_name invalid: " + ref_name
+			throw 'ref_name invalid: ' + ref_name
 		}
 
-		target = forcePrefix + parts[0] + "." + parts[1]
+		target = forcePrefix + parts[0] + '.' + parts[1]
 		setTarget(target)
-	} catch (error : any) {
-		setFailed(error);
+	} catch (error: any) {
+		setFailed(error)
 	}
 }
 
