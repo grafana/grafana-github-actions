bb51dfeaeef835b738f306a43527da068eebfdac
dsotirakis <dimitrios.sotirakis@grafana.com>
2023-06-12 11:07:50 +0300

    Add more instructions to failing backports

--- backport/backport.ts
+++ backport/backport.ts
@@ -204,6 +204,8 @@ const getFailedBackportCommentBody = ({
 		`git switch --create ${head} origin/${base}`,
 		'# Cherry-pick the merged commit of this pull request and resolve the conflicts',
 		`git cherry-pick -x ${commitToBackport}`,
+		'# When the conflicts are resolved, stage and commit the changes',
+		`git add . && git commit --no-edit`,
 		'# Push it to GitHub',
 		`git push --set-upstream origin ${head}`,
 		`git switch main`,
