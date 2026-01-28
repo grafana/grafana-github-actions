da7c6bff93693f7a61f3ac107eb5e5e04729d54d
grambbledook <denis.vodopianov@grafana.com>
2026-01-28 17:49:03 +0100

    add compiled stuff

---
+++ gha-otel-export/dist/272.index.js
@@ -0,0 +1,82 @@
+export const id = 272;
+export const ids = [272];
+export const modules = {
+
+/***/ 1223:
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.execAsync = void 0;
+const child_process = __webpack_require__(5317);
+const util = __webpack_require__(9023);
+exports.execAsync = util.promisify(child_process.exec);
+//# sourceMappingURL=execAsync.js.map
+
+/***/ }),
+
+/***/ 7272:
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+exports.getMachineId = void 0;
+const execAsync_1 = __webpack_require__(1223);
+const api_1 = __webpack_require__(3914);
+async function getMachineId() {
+    try {
+        const result = await (0, execAsync_1.execAsync)('ioreg -rd1 -c "IOPlatformExpertDevice"');
+        const idLine = result.stdout
+            .split('\n')
+            .find(line => line.includes('IOPlatformUUID'));
+        if (!idLine) {
+            return undefined;
+        }
+        const parts = idLine.split('" = "');
+        if (parts.length === 2) {
+            return parts[1].slice(0, -1);
+        }
+    }
+    catch (e) {
+        api_1.diag.debug(`error reading machine id: ${e}`);
+    }
+    return undefined;
+}
+exports.getMachineId = getMachineId;
+//# sourceMappingURL=getMachineId-darwin.js.map
+
+/***/ })
+
+};
+
+//# sourceMappingURL=272.index.js.map
